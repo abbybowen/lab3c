cb372a56f648f703099b4399bc9e9395b58a41ee
abbybowen <aeb6095@psu.edu>
2020-09-17 18:24:02 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,39 @@
+// Author: Abigail Bowen
+// Collaborator: Nathan Knox nhk5053@psu.edu
+// Collaborator: Aaryan Darshin Bavishi aqb6298@psu.edu
+// Section: 6
+// Breakout: 10
+
+#include <stdio.h>
+#include <math.h>
+#include <readline/readline.h>
+#include <stdlib.h>
+
+   int sum_n(int n) {
+    if (n == 0){
+     return 0;
+   }
+    else if (n == 1){
+     return 1;
+   }
+    else {
+      return (n) + sum_n(n - 1);
+    }
+     
+  }
+  
+   void print_threetimes(const char *s) {
+    printf("%s\n",s);
+    printf("%s\n",s);
+    printf("%s\n",s);
+    return;
+  }
+
+
+ int main(void) {
+   int n = atoi(readline("Enter an int: "));
+   printf("sum is %d.\n",sum_n(n));
+   char *s = readline("Enter a string: ");
+   print_threetimes(s);
+
+ }
